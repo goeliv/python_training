043602c79131ad3bb563b54990bcef0ef5b901e7
Elena Golyk <golykelena@gmail.com>
2016-09-30 12:50:58 +0300

    Revert "Refactoring"
    This reverts commit a5448f7893ef418616d7c815e88689e68c718c3d.

--- test_add_group.py
+++ test_add_group.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+from selenium.webdriver.common.action_chains import ActionChains
+import time, unittest
 
 def is_alert_present(wd):
     try:
@@ -17,22 +18,15 @@ class test_add_group(unittest.TestCase):
     def test_test_add_group(self):
         success = True
         wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd)
-        self.creation_group(wd)
-        self.return_to_groups_page(wd)
-        self.logout(success)
-
-    def logout(self, success):
-        self.assertTrue(success)
-
-    def return_to_groups_page(self, wd):
-        wd.find_element_by_link_text("groups").click()
-
-    def creation_group(self, wd):
-        # init group creation
+        wd.get("http://localhost/addressbook/group.php")
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
         wd.find_element_by_name("new").click()
-        # fill group firm
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("dsf")
@@ -42,21 +36,10 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("sf")
-        # submit group creation
         wd.find_element_by_name("submit").click()
-
-    def login(self, wd):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/group.php")
-
+        wd.find_element_by_link_text("groups").click()
+        self.assertTrue(success)
+    
     def tearDown(self):
         self.wd.quit()
 
