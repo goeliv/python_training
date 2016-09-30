04223e12a06a91e7f3958a73b1dbfd789328f5c4
Elena Golyk <golykelena@gmail.com>
2016-09-30 13:07:55 +0300

    New add_group

--- test_add_group.py
+++ test_add_group.py
@@ -18,7 +18,7 @@ class test_add_group(unittest.TestCase):
     def test_test_add_group(self):
         success = True
         wd = self.wd
-        wd.get("http://localhost/addressbook/group.php")
+        wd.get("http://localhost/addressbook/")
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -26,18 +26,19 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("dsf")
+        wd.find_element_by_name("group_name").send_keys("dfgdfg")
         wd.find_element_by_name("group_header").click()
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("sdf")
+        wd.find_element_by_name("group_header").send_keys("dfgdfgd")
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("sf")
+        wd.find_element_by_name("group_footer").send_keys("dfgdfgdfg")
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("groups").click()
+        wd.find_element_by_link_text("group page").click()
         self.assertTrue(success)
     
     def tearDown(self):
