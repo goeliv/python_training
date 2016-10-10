8e611c71e4b282d72d55f21023caf4413671247d
goeliv <golykelena@gmail.com>
2016-10-10 23:21:59 +0300

    Выделен вспомогат. класс SessionHelper (пока только группы)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,10 +13,10 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="gkgk", middlename="gkgkg", lastname="gkhgkg", nick="gkgkgk", title="gkgk", company="gkgkjgkgk", address="gkgkjgkgk", home_tel="777", mob_tel="888",
                             work_tel="999", fax="777", email="2532@AMAI.COM", email2="2515832@AMAI.COM", homepage="ppp.com",birthday="1989"))
-    app.logout()
+    app.session.logout()
 
 
 
