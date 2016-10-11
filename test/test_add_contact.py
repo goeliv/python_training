3c429516b617b38850002f2bee9de0ce0207f3b5
Elena Golyk <golykelena@gmail.com>
2016-10-11 17:01:22 +0300

    task6

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,6 @@
 # -*- coding: utf-8 -*-
-import pytest
-from fixture.application import Application
+
 from model.contact import Contact
-
-
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_contact(app):
