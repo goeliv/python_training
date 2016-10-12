b55f9f5a034c665a31ce7e38756d0dddfbc3a834
Elena Golyk <golykelena@gmail.com>
2016-10-12 15:05:02 +0300

    task6 c настройками на 2 мозиллы

--- fixture/application.py
+++ fixture/application.py
@@ -2,11 +2,13 @@ from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
 from fixture.contacts import ContactsHelper
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 class Application:
 
     def __init__(self):
-        self.wd = WebDriver()
+        binary = FirefoxBinary('C:\\Program Files\\Mozilla Firefox\\firefox.exe')
+        self.wd = WebDriver(firefox_binary=binary)
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
         self.group = GroupHelper (self)
