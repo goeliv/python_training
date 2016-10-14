fa62f4a7dc957d29403300b3c0d5aebb8458ebca
goeliv <golykelena@gmail.com>
2016-10-14 23:45:03 +0300

    task7 улучшенный код
    рефакторинг

--- fixture/group.py
+++ fixture/group.py
@@ -17,6 +17,13 @@ class GroupHelper:
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
+        self.fill_form(group)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.return_to_groups_page()
+
+    def fill_form(self, group):
+        wd = self.app.wd
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -26,9 +33,6 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
 
     def delete_first_group(self):
         wd = self.app.wd
@@ -50,14 +54,6 @@ class GroupHelper:
         self.open_groups_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_name("edit").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.fill_form(group)
         wd.find_element_by_name("update").click()
         self.return_to_groups_page()
