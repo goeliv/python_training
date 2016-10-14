ec00d1f539357f736296fe521c611610288672b4
goeliv <golykelena@gmail.com>
2016-10-14 12:49:34 +0300

    after update gitingore

---
+++ test/test_add_group.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from fixture.application import Application
+from model.group import Group
+
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.creat(Group(name="dfgdfg", header="dfgdfgd", footer="dfgdfgdfg"))
+    app.session.logout()
+
+def test_add_empty_group(app):
+    app.session.login(username="admin", password="secret")
+    app.group.creat(Group(name="", header="", footer=""))
+    app.session.logout()
+
+
+
