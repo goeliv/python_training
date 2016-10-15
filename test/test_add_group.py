8085edbaa609e5a40a787ef67cc0cef6c94ac42b
goeliv <golykelena@gmail.com>
2016-10-15 12:27:30 +0300

    task8 логин-логаут общие

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,11 +5,6 @@ from fixture.application import Application
 from model.group import Group
 
 
-@pytest.fixture
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 def test_add_group(app):
     app.group.creat(Group(name="dfgdfg", header="dfgdfgd", footer="dfgdfgdfg"))
