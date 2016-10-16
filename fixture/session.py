8dcc2bef7fa50961fe588fb27a70b542f7edd5fb
goeliv <golykelena@gmail.com>
2016-10-16 12:20:37 +0300

    task8

--- fixture/session.py
+++ fixture/session.py
@@ -10,6 +10,19 @@ class SessionHelper:
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
 
+    def ensure_logout (self):
+        wd = self.app.wd
+        if self.is_logged_in():
+            self.logout()
+
+    def is_logged_in(self):
+        wd = self.app.wd
+        return len(wd.find_elements_by_link_text("Logout")) > 0
+
+    def is_logged_in_as(self, username):
+        wd = self.app.wd
+        return wd.find_element_by_xpath ("//div/div[1]/form/b").text =="("+username+")"
+
     def login(self, username, password):
         wd = self.app.wd
         self.app.open_home_page()
@@ -20,3 +33,14 @@ class SessionHelper:
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+
+    def ensure_login (self, username, password):
+        wd = self.app.wd
+        if self.is_logged_in():
+            if self.is_logged_in_as(username):
+                return
+            else:
+                self.logout()
+        self.login(username,password)
+
