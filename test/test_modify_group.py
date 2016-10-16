486b662473d69a0e2eeaac15f5ecd318c07aefd5
goeliv <golykelena@gmail.com>
2016-10-16 16:36:17 +0300

    task9

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,4 +1,6 @@
 from model.group import Group
 
 def test_modify_first_group(app):
-    app.group.modify(Group(name="new", header="new", footer="new"))
+    if app.group.count()==0:
+        app.group.create(Group(name="1", header="1", footer="1"))
+        app.group.modify(Group(name="new", header="new", footer="new"))
