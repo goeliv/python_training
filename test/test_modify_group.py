dc37bfd2a75c571b2ad4dd7386ce57496d98c93a
Elena Golyk <golykelena@gmail.com>
2016-10-17 18:32:30 +0300

    Revert "task9"
    This reverts commit 486b662473d69a0e2eeaac15f5ecd318c07aefd5.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,6 +1,4 @@
 from model.group import Group
 
 def test_modify_first_group(app):
-    if app.group.count()==0:
-        app.group.create(Group(name="1", header="1", footer="1"))
-        app.group.modify(Group(name="new", header="new", footer="new"))
+    app.group.modify(Group(name="new", header="new", footer="new"))
