0e3fd9a278802f1cbfe048859c8c8915db36e1cb
Elena Golyk <golykelena@gmail.com>
2016-10-24 13:05:03 +0300

    task11 удаление групп, сравнение по имени и ИД

--- model/group.py
+++ model/group.py
@@ -6,6 +6,14 @@ class Group:
         self.footer=footer
         self.id=id
 
+    def __repr__(self):
+        return "%s:%s" % (self.id, self.name)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.name == other.name
+
+
+
 
 
 
