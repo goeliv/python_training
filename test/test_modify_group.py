04bd11cf95abd7f4488a02ce5d414655e00ea898
Elena Golyk <golykelena@gmail.com>
2016-10-24 17:24:54 +0300

    task11 все по групам реализовано

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,4 +1,11 @@
 from model.group import Group
 
 def test_modify_first_group(app):
+    old_groups=app.group.get_group_list()
+    group = Group(name="new", header="new", footer="new")
+    group.id=old_groups[0].id
     app.group.modify(Group(name="new", header="new", footer="new"))
+    new_groups=app.group.get_group_list()
+    assert len(old_groups)== len (new_groups)
+    old_groups[0]= group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
