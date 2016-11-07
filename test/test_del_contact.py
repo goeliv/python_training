6dd307602b9e9719fe5df3941a7cb3e7e6e323f8
Elena Golyk <golykelena@gmail.com>
2016-11-07 16:47:06 +0200

    task13_updd

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -5,10 +5,10 @@ def test_delete_some_contact(app):
     if app.contacts.count() == 0:
         app.contacts.create(Contact(firstname="test", middlename="test", lastname="gkhgkg", nick="gkgkgk", title="gkgk", company="gkgkjgkgk", address="gkgkjgkgk", home_tel="777", mob_tel="888",
                                 work_tel="999", fax="777", email="2532@AMAI.COM", email2="2515832@AMAI.COM", homepage="ppp.com", birthday="1989"))
-        old_contacts = app.contacts.get_contact_list()
-        index = randrange(len(old_contacts))
-        app.contacts.delete_first_contact(index)
-        new_contacts = app.contacts.get_contact_list()
-        assert len(old_contacts) - 1 == len(new_contacts)
-        old_contacts[index:index+1] = []
-        assert old_contacts == new_contacts
+    old_contacts = app.contacts.get_contact_list()
+    index = randrange(len(old_contacts))
+    app.contacts.delete_first_contact(index)
+    new_contacts = app.contacts.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+    old_contacts[index:index+1] = []
+    assert old_contacts == new_contacts
