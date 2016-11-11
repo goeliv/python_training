8d33a2b76854bbd82ba72f20a52c6a3001d10036
Elena Golyk <golykelena@gmail.com>
2016-11-11 18:29:17 +0200

    14_3

--- test/Test_phones.py
+++ test/Test_phones.py
@@ -6,7 +6,7 @@ def test_phones_on_home_page(app):
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 def test_phones_on_view_page(app):
-    contact_from_view_page = app.contacts.get_contact_form_view_page(0)
+    contact_from_view_page = app.contacts.get_contact_from_view_page(0)
     contact_from_edit_page = app.contacts.get_contact_info_from_edit_page(0)
     assert contact_from_view_page.home_tel == contact_from_edit_page.home_tel
     assert contact_from_view_page.work_tel == contact_from_edit_page.work_tel
