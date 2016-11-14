2a3c47cb5bb238490a057167dcddf8268d74ebaa
Elena Golyk <golykelena@gmail.com>
2016-11-14 14:29:59 +0200

    15_2

--- fixture/contacts.py
+++ fixture/contacts.py
@@ -121,7 +121,7 @@ class ContactsHelper:
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
                 all_phones = cells[5].text
                 all_emails = cells[4].text
-                self.rows_cache.append(Contact(firstname=firstname, lastname=lastname, id=id, all_phones_from_home_page=all_phones,all_email_from_home_page=all_emails ))
+                self.rows_cache.append(Contact(firstname=firstname, lastname=lastname, id=id, all_phones_from_home_page=all_phones,all_emails_from_home_page=all_emails ))
         return list(self.rows_cache)
 
     def open_contact_to_edit_by_index(self, index):
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,27 +3,30 @@ import pytest
 
 from fixture.application import Application
 from model.group import Group
+import pytest
+import random
+import string
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
+testdata = [Group(name="", header="", footer="")] + [
+        Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+        for i in range(5)
+]
 
-def test_add_group(app):
-    old_groups = app.group.get_group_list ()
-    group = Group(name="dfgdfg", header="dfgdfgd", footer="dfgdfgdfg")
-    app.group.create(group)
-    new_groups = app.group.get_group_list ()
-    assert len (old_groups) + 1 == len(new_groups)
-    old_groups.append (group)
-    assert sorted(old_groups,key=Group.id_or_max) == sorted (new_groups,key=Group.id_or_max)
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
+        old_groups = app.group.get_group_list ()
+        app.group.create(group)
+        new_groups = app.group.get_group_list ()
+        assert len (old_groups) + 1 == len(new_groups)
+        old_groups.append (group)
+        assert sorted(old_groups,key=Group.id_or_max) == sorted (new_groups,key=Group.id_or_max)
 
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list ()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
