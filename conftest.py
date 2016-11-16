37241f0d109eda49e265a12a31eab18de73ef5a7
Elena Golyk <golykelena@gmail.com>
2016-11-16 13:09:58 +0200

    task_16upd

--- conftest.py
+++ conftest.py
@@ -9,9 +9,9 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
+    browser = request.config.getoption("--browser")
+    base_url = request.config.getoption("--baseUrl")
     if fixture is None:
-        browser = request.config.getoption("--browser")
-        base_url = request.config.getoption("--baseUrl")
         fixture = Application(browser=browser, base_url=base_url)
     else:
         if not fixture.is_valid ():
