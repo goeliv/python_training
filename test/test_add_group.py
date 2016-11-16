074b06bc64ca97a5a89956da9bf95715e241d751
Elena Golyk <golykelena@gmail.com>
2016-11-16 18:48:03 +0200

    18_1

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,19 +1,8 @@
 # -*- coding: utf-8 -*-
-from fixture.application import Application
 from model.group import Group
 import pytest
-import random
-import string
+from data.add_group import testdata
 
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-testdata = [Group(name="", header="", footer="")] + [
-        Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-        for i in range(5)
-]
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
