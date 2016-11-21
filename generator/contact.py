d90804238750090c80e04626126f3ea84de6d3c2
Elena Golyk <golykelena@gmail.com>
2016-11-21 18:32:54 +0200

    task_18

---
+++ generator/contact.py
@@ -0,0 +1,44 @@
+from model.contact import Contact
+import random
+import string
+import os.path
+import json
+import getopt
+import sys
+
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
+except getopt.Getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n= int(a)
+    elif o == "-f":
+        f = a
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+def random_string1(prefix, maxlen):
+    symbols = string.digits
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(firstname=random_string("firstname", 10), middlename=random_string("middlename", 20), lastname=random_string("lastname", 20),
+                    nick=random_string("nick", 20), title=random_string("title", 20), company=random_string("company", 20), address=random_string("address", 20),
+                    home_tel=random_string1("home_tel", 20),  mob_tel=random_string1("mob_tel", 20),  work_tel=random_string1("work_tel", 20),  fax=random_string1("fax", 20),
+                    email=random_string("email", 20), email2=random_string("email", 20), homepage=random_string("homepage", 20), birthday=random_string("birthday", 20))
+        for i in range(n)
+]
+
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
