3b4f920e7c49f9dc04f91a0d529553ef5d11513f
Elena Golyk <golykelena@gmail.com>
2016-11-24 19:06:31 +0200

    20_10

--- conftest.py
+++ conftest.py
@@ -30,6 +30,7 @@ def stop(request):
 def pytest_addoption(parser):
     parser.addoption("--browser", action="store", default="firefox")
     parser.addoption("--target", action="store", default="target.json")
+    parser.addoption("--check_ui", action="store_true")
 
 def pytest_generate_tests(metafunc):
     for fixture in metafunc.fixturenames:
@@ -65,5 +66,9 @@ def load_config(file):
             target = json.load(f)
     return target
 
+
+@pytest.fixture
+def check_ui(request):
+    return request.config.getoption("--check_ui")
 
 
