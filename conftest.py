00c155f62dfb19744d934166c3814c708fcbfb0a
Elena Golyk <golykelena@gmail.com>
2016-11-24 15:05:33 +0200

    20_6

--- conftest.py
+++ conftest.py
@@ -51,7 +51,7 @@ def load_from_json(file):
 @pytest.fixture (scope = "session")
 def db(request):
     db_config = load_config(request.config.getoption("--target"))['db']
-    dbfixture = DbFixture(host=db_config['host'], user=db_config['user'], password=db_config['password'])
+    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
     def fin():
         dbfixture.destroy()
     request.addfinalizer(fin)
