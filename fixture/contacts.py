c064cf58ee584df56ea6cb1d5e5f3c5c2790cd08
goeliv <golykelena@gmail.com>
2016-11-26 23:48:11 +0200

    task_14

--- fixture/contacts.py
+++ fixture/contacts.py
@@ -131,10 +131,11 @@ class ContactsHelper:
                 cells = row.find_elements_by_tag_name("td")
                 firstname = cells[2].text
                 lastname = cells[1].text
+                address = cells[4].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
                 all_phones = cells[5].text
                 all_emails = cells[4].text
-                self.rows_cache.append(Contact(firstname=firstname, lastname=lastname, id=id, all_phones_from_home_page=all_phones,all_emails_from_home_page=all_emails ))
+                self.rows_cache.append(Contact(firstname=firstname, lastname=lastname, id=id, address=address, all_phones_from_home_page=all_phones,all_emails_from_home_page=all_emails ))
         return list(self.rows_cache)
 
     def open_contact_to_edit_by_index(self, index):
---
+++ test/test_address.py
@@ -0,0 +1,9 @@
+import re
+from random import randrange
+
+def test_lastname_on_home_page(app):
+    contacts = app.contacts.get_contact_list()
+    index = randrange(len(contacts))
+    contact_from_home_page = app.contacts.get_contact_list()[index]
+    contact_from_edit_page = app.contacts.get_contact_from_edit_page(index)
+    assert contact_from_home_page.address == contact_from_edit_page.address
